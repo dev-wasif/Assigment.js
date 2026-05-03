221d912f8766127ad66f8274e4810a7ef52001d4
dev-wasif <wasifalam1919@gmail.com>
2026-05-03 10:48:33 +0500

    Assigment(js)

--- chapters/ArrayMethod.js
+++ chapters/ArrayMethod.js
@@ -1,96 +1,26 @@
+// Array Methods Version
+
 var arr9 = [1, 2, 2, 4, 6, 6, 9, 7, 8, 7];
 var arr10 = [38, 78, 65, 34, 3, 8, 53];
 
-
-// Question 1
-
-var unique = [];
-
-for (var i = 0; i < arr9.length; i++) {
-    var isDuplicate = false;
-
-    for (var j = 0; j < unique.length; j++) {
-        if (arr9[i] === unique[j]) {
-            isDuplicate = true;
-            break;
-        }
-    }
-
-    if (!isDuplicate) {
-        unique.push(arr9[i]);
-    }
-}
-
+// Question 1 – Unique values
+var unique = [...new Set(arr9)];
 console.log("Unique:", unique);
 
-
-// Question 2
-
-var duplicates = [];
-
-for (var i = 0; i < arr9.length; i++) {
-    for (var j = i + 1; j < arr9.length; j++) {
-
-        if (arr9[i] === arr9[j]) {
-
-            var already = false;
-
-            for (var k = 0; k < duplicates.length; k++) {
-                if (duplicates[k] === arr9[i]) {
-                    already = true;
-                    break;
-                }
-            }
-
-            if (!already) {
-                duplicates.push(arr9[i]);
-            }
-        }
-    }
-}
-
+// Question 2 – Duplicates
+var duplicates = [...new Set(arr9.filter((item, index) => arr9.indexOf(item) !== index))];
 console.log("Duplicates:", duplicates);
 
-
-// Question3
-
-var smallest = arr10[0];
-
-for (var i = 1; i < arr10.length; i++) {
-    if (arr10[i] < smallest) {
-        smallest = arr10[i];
-    }
-}
-
+// Question 3 – Smallest number
+var smallest = Math.min(...arr10);
 console.log("Smallest:", smallest);
 
-
-// Question4
-
-var largest = arr10[0];
-
-for (var i = 1; i < arr10.length; i++) {
-    if (arr10[i] > largest) {
-        largest = arr10[i];
-    }
-}
-
+// Question 4 – Largest number
+var largest = Math.max(...arr10);
 console.log("Largest:", largest);
 
-
-// Question No 5
-
-var max = arr10[0];
-var secondMax = arr10[0];
-
-for (var i = 1; i < arr10.length; i++) {
-
-    if (arr10[i] > max) {
-        secondMax = max;
-        max = arr10[i];
-    } else if (arr10[i] > secondMax && arr10[i] !== max) {
-        secondMax = arr10[i];
-    }
-}
+// Question 5 – Second Largest
+var secondMax = [...new Set(arr10)]
+    .sort((a, b) => b - a)[1];
 
 console.log("Second Largest:", secondMax);
